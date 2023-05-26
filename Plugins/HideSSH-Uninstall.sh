56284e2c37255f20663ef372a509bf59f2519927
Gege Desembri <62755680+GegeDesembri@users.noreply.github.com>
2023-05-26 19:05:13 +0700

    Create HideSSH-Uninstall.sh

---
+++ Plugins/HideSSH-Uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf /usr/local/bin/add-ssh-user
+rm -rf /usr/local/bin/del-ssh-user
+rm -rf /usr/local/bin/cek-ssh-user
+rm -rf /usr/local/bin/add-vmess-user
+rm -rf /usr/local/bin/del-vmess-user
+rm -rf /usr/local/bin/cek-vmess-user
+rm -rf /usr/local/bin/add-trojan-user
+rm -rf /usr/local/bin/del-trojan-user
+rm -rf /usr/local/bin/cek-trojan-user
+
+echo -e "HideSSH Plugin Uninstalled"
